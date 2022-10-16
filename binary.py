cc41e73375c8208d96e601b4d76a56105059b046
choudhurimanash <manashchoudhuri4@gmail.com>
2022-10-16 22:36:51 +0530

    add more code to read image and convert to binary image

--- binary.py
+++ binary.py
@@ -2,4 +2,14 @@
 
 import cv2
 #  read the image file as gray scale image
- 
+img = cv2.imread('m01.jpg',2)
+
+# set the threshold value
+ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+# show the image
+cv2.imshow("Binary Image",bw_img)
+
+# waitKey and destroyAllWindows method to keep our window always open until we press any key or close our window 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
