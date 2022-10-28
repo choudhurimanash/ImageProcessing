791fd8f714e622169b66fe84acde2d457bbd955e
choudhurimanash <manashchoudhuri4@gmail.com>
2022-10-28 21:28:42 +0530

    python program to convert an rgb image to grayscale image

--- GrayScaleImage/grayScale.py
+++ GrayScaleImage/grayScale.py
@@ -2,3 +2,15 @@
 
 import cv2
 
+#read the image
+img_rgb = cv2.imread('m02.jpg')
+
+# Use the cvtColor() function to grayscale the image
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+#show the image  
+cv2.imshow('Gray image', img_gray)
+
+# waitKey and destroyAllWindows method to keep our window always open until we press any key or close our window   
+cv2.waitKey(0)
+cv2.destroyAllWindows()
